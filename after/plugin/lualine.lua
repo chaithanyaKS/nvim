62d720f957e1807e36f90b7b00965594660277ab
chaithanya <chaithanya2934@gmail.com>
2023-08-11 22:58:47 +0530

    added tabline

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,8 +1,16 @@
-require("lualine").setup{
-	options = {
-		icons_enabled = false,
-		theme = "onedark",
-		component_separators = "|",
-		section_separators = "",
-	},
+require("lualine").setup {
+    options = {
+        icons_enabled = false,
+        theme = "onedark",
+        component_separators = "|",
+        section_separators = "",
+    },
+    tabline = {
+        lualine_a = {'buffers' },
+        lualine_b = {},
+        lualine_c = { },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
+    }
 }
