8f6de042abe9e82b31da542e1097184a80a73686
chaithanya <chaithanya2934@gmail.com>
2023-08-15 21:36:44 +0530

    small changes

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -6,11 +6,20 @@ require("lualine").setup {
         section_separators = "",
     },
     tabline = {
-        lualine_a = {'buffers' },
+        lualine_a = { 'buffers' },
         lualine_b = {},
-        lualine_c = { },
+        lualine_c = {},
         lualine_x = {},
         lualine_y = {},
         lualine_z = {}
-    }
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
+
 }
